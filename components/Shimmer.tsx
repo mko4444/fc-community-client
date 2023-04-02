82f0a3d85bdb37c4813074028e4cf46cef704f5b
Matthew <mrk443@stern.nyu.edu>
2023-04-02 16:37:34 -0700

    added a loading shimmer component

---
+++ components/Shimmer.tsx
@@ -0,0 +1,25 @@
+export function Shimmer({
+  width = "100%",
+  height = "20px",
+  flex = false,
+  borderRadius = 4,
+  margin = 0,
+  isLoading = true,
+  children,
+}: ShimmerProps): JSX.Element {
+  return isLoading ? (
+    <div className="shimmer-loader" style={{ flex: flex ? 1 : "none", width, height, borderRadius, margin }} />
+  ) : (
+    children
+  );
+}
+
+export type ShimmerProps = {
+  width?: string | number;
+  height?: string | number;
+  flex?: boolean;
+  borderRadius?: number;
+  margin?: string | number;
+  isLoading: boolean;
+  children: JSX.Element | JSX.Element[];
+};
