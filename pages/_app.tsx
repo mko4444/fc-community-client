d51d9438a9cfdc2c6b01b850a75c3d3170c5b392
Matthew <mrk443@stern.nyu.edu>
2023-04-04 01:22:34 -0700

    scroll to top on page load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,16 @@ import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 
 import "styles/index.scss";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter();
+
+  useEffect(() => {
+    document.body.scrollTop = 0;
+  }, [asPath]);
+
   return (
     <SWRConfig
       value={{
